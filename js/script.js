62dc68db4e108d45e5ff9591c7e3cc27a0e9d61a
Katy Solovewicz <katy@solovewicz.com>
2020-01-24 11:18:25 -0800

    add comments to js file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Business at the top
 
+// Check user input for "3"
+function checkThree(number){
+	if (number.includes(3)){
+		output = "I'm sorry, Dave, I can't do that.";
+		return output;
+	}
+	else {
+		output = checkTwo(number);
+		return output;
+	}
+}
+
+// Check user input for "2"
 function checkTwo(number){
 	if (number.includes(2)){
 		output = "boop";
@@ -11,6 +24,7 @@ function checkTwo(number){
 	}
 }
 
+// Check user input for "1"
 function checkOne(number){
 	if (number.includes(1)){
 		output = "beep";
@@ -22,7 +36,7 @@ function checkOne(number){
 	}
 }
 
-
+// Output range of number from 0 until the user's input
 function extract(number){
 	var range = [];
 	for (var i = 0; i < number; i++){
@@ -32,7 +46,6 @@ function extract(number){
 	return range;
 };
 
-
 // UI at the bottom
 $(document).ready(function(){
 	$("form").submit(function(event){
@@ -41,7 +54,7 @@ $(document).ready(function(){
 			return alert("Please submit a number.");
 		}
 		else {
-			var output = checkTwo(input);
+			var output = checkThree(input);
 			console.log("output: " + output);
 		}
 	});

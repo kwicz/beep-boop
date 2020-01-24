bdaf212d2886844c1a9a46e999097dd2c280e92b
Katy Solovewicz <katy@solovewicz.com>
2020-01-24 10:18:25 -0800

    return error if no input, or return input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
+// Business at the top
 
+
+
+// UI at the bottom
+$(document).ready(function(){
+	$("form").submit(function(event){
+		var input = $("input").val();
+		if (!input){
+			return alert("Please submit a number.");
+		}
+		else {
+			console.log("input: " + input);
+		}
+	});
 });
